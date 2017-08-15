fb740e8b5fbd419d318338bba39348e3bd4b85fe
Jonathan Hefner <jonathan.hefner@gmail.com>
2017-08-15 14:24:22 -0500

    Remove default test

--- test/gorge_test.rb
+++ test/gorge_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
 class GorgeTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Gorge::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
